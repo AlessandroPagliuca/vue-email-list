aa597cfa79474d55e35794ec21fa39c039198937
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-11 15:27:02 +0200

    Start project

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+*/
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            titolo: 'Vue Email List',
+            randomMail: [],
+            basePath:'https://flynn.boolean.careers/exercises/api/'
+        }
+    },
+    methods:{
+        getData(){
+
+            this.randomMail = [];
+            for(let i = 0; i < 10; i++){
+                axios.get(this.basePath+'random/mail').then((result)=>{
+                    console.log(result.data.response);
+                    this.randomMail.push(result.data.response);
+                });
+            }
+           
+        }
+    },
+    mounted(){
+        this.getData();
+
+    },
+}).mount('#app');
